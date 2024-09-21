eaaa22b12638a28e472ff408b4394be0d2687632
zeakd <artydeveloperduck@gmail.com>
2024-09-22 00:07:21 +0900

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import pluginJs     from '@eslint/js';
+import stylistic    from '@stylistic/eslint-plugin';
+import arca         from 'eslint-plugin-arca';
+import pluginImport from 'eslint-plugin-import';
+import globals      from 'globals';
+import tseslint     from 'typescript-eslint';
+
+export default [
+  {
+    files:   ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignores: [
+      '**/node_modules',
+      '**/.next',
+      '**/dist',
+      '**/out',
+      '**/_*',
+    ],
+  },
+  tseslint.configs.base,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  stylistic.configs['recommended-flat'],
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+      '@arca':      arca,
+      '@import':    pluginImport,
+    },
+    rules: {
+      // stylistic
+      '@stylistic/member-delimiter-style': ['error', {
+        multiline: {
+          delimiter:   'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter:   'semi',
+          requireLast: false,
+        },
+      }],
+      '@stylistic/semi':                 ['error', 'always'],
+      '@stylistic/indent':               ['error', 2],
+      '@stylistic/quote-props':          ['error', 'as-needed'],
+      '@stylistic/no-multi-spaces':      ['error', { exceptions: { ImportDeclaration: true, TSTypeAnnotation: true } }],
+      '@stylistic/key-spacing':          ['error', { afterColon: true, beforeColon: false, align: 'value' }],
+      '@stylistic/object-curly-newline': ['error', {
+        ImportDeclaration: { multiline: true, consistent: true },
+      }],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+
+      // arca
+      '@arca/import-align': ['error', { collapseExtraSpaces: true }],
+
+      // import
+      '@import/order': ['error', {
+        'newlines-between': 'always',
+        groups:             ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        alphabetize:        { order: 'asc', caseInsensitive: true },
+      }],
+    },
+  },
+];
